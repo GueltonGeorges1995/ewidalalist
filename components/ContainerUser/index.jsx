f52f3d9fb7cc0953bb9058b947061b4c6baa5576
GueltonGeorges1995 <49947507+GueltonGeorges1995@users.noreply.github.com>
2021-04-29 23:19:44 +0200

    style on button added on containerUser comp

--- components/ContainerUser/index.jsx
+++ components/ContainerUser/index.jsx
@@ -4,13 +4,18 @@ import styles from "./ContainerUser.module.css";
 
 export default function ContainerUser({ user, handleDeleteUser }) {
   return (
-    <div key={user.id} className="d-flex just-cont-around">
+    <div key={user.id} className={styles.userRow}>
       <Link key={user.id} href={`/ninjas/${user.id}`}>
         <a className={styles.single}>
           <h3>{user.name}</h3>
         </a>
       </Link>
-      <button onClick={() => handleDeleteUser(user.id)}>X</button>
+      <button
+        onClick={() => handleDeleteUser(user.id)}
+        className={styles.deleteButton}
+      >
+        X
+      </button>
     </div>
   );
 }
